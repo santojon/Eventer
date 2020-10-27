1922e192da807844f8d3540248438a3dd5e24646
Jonathan Santos <santojon5@gmail.com>
2020-10-26 23:18:24 -0300

    Change constructors, make open

--- library/src/main/java/com/santojon/eventer/core/manager/EventManager.kt
+++ library/src/main/java/com/santojon/eventer/core/manager/EventManager.kt
@@ -9,46 +9,50 @@ import io.reactivex.rxjava3.subjects.PublishSubject
  * Used to manage events using [EventStream]
  */
 open class EventManager<T : Any>() {
-    private val subscribeOn: Scheduler? = null
-    private val observeOn: Scheduler? = null
+    protected var subscribeOn: Scheduler? = null
+    protected var observeOn: Scheduler? = null
 
-    /**¬
+    /**
      * Alternative constructors
      */
-    constructor(subscribeOn: Scheduler? = null, observeOn: Scheduler? = null) : this()
+    constructor(subscribeOn: Scheduler? = null, observeOn: Scheduler? = null) : this() {
+        this.subscribeOn = subscribeOn
+        this.observeOn = observeOn
+    }
+
     constructor(subscribeOn: Int?, observeOn: Int?) : this(
         EventSchedulers.from(subscribeOn),
         EventSchedulers.from(observeOn)
     )
 
     // Events subject
-    private var evs: PublishSubject<T>? = PublishSubject.create()
+    protected var evs: PublishSubject<T>? = PublishSubject.create()
 
     /**
      * Add event to Subject
      */
-    fun addEvent(event: T?) = event?.let { evs?.onNext(event) }
-    fun publish(event: T?) = addEvent(event)
-    fun sendEvent(event: T?) = addEvent(event)
+    open fun addEvent(event: T?) = event?.let { evs?.onNext(event) }
+    open fun publish(event: T?) = addEvent(event)
+    open fun sendEvent(event: T?) = addEvent(event)
 
     /**
      * Add events to Subject
      */
-    fun addEvents(vararg events: T?) = events.forEach { event -> addEvent(event) }
-    fun publishMany(vararg events: T?) = addEvents(*events)
-    fun sendEvents(vararg events: T?) = addEvents(*events)
+    open fun addEvents(vararg events: T?) = events.forEach { event -> addEvent(event) }
+    open fun publishMany(vararg events: T?) = addEvents(*events)
+    open fun sendEvents(vararg events: T?) = addEvents(*events)
 
     /**
      * Return stream of events
      */
-    fun asStream(): EventStream<T>? = EventStream(evs, subscribeOn, observeOn)
-    val events: EventStream<T>? = asStream()
-    val stream: EventStream<T>? = asStream()
+    open fun asStream(): EventStream<T>? = EventStream(evs, subscribeOn, observeOn)
+    open val events: EventStream<T>? = asStream()
+    open val stream: EventStream<T>? = asStream()
 
     /**
      * Clear the eventStream
      */
-    fun clear() {
+    open fun clear() {
         evs = null
         evs = PublishSubject.create()
     }
--- library/src/main/java/com/santojon/eventer/core/stream/EventStream.kt
+++ library/src/main/java/com/santojon/eventer/core/stream/EventStream.kt
@@ -9,11 +9,22 @@ import io.reactivex.rxjava3.kotlin.withLatestFrom
 import java.util.concurrent.TimeUnit
 import kotlin.reflect.KClass
 
-class EventStream<T : Any>(
-    val observable: Observable<T>?,
-    private val subscribeOn: Scheduler? = null,
-    private val observeOn: Scheduler? = null
-) {
+open class EventStream<T : Any>(open val observable: Observable<T>?) {
+    protected var subscribeOn: Scheduler? = null
+    protected var observeOn: Scheduler? = null
+
+    /**
+     * Alternative constructors
+     */
+    constructor(
+        observable: Observable<T>?,
+        subscribeOn: Scheduler? = null,
+        observeOn: Scheduler? = null
+    ) : this(observable) {
+        this.subscribeOn = subscribeOn
+        this.observeOn = observeOn
+    }
+
     constructor(observable: Observable<T>?, subscribeOn: Int?, observeOn: Int?) : this(
         observable,
         EventSchedulers.from(subscribeOn),
@@ -23,7 +34,7 @@ class EventStream<T : Any>(
     /**
      * Subscribe to get stream
      */
-    fun subscribe(
+    open fun subscribe(
         onNext: (T?) -> Unit,
         onError: (Throwable?) -> Unit,
         onComplete: () -> Unit
@@ -46,28 +57,32 @@ class EventStream<T : Any>(
         }
     }
 
-    fun subscribe(onNext: (T?) -> Unit, onError: (Throwable?) -> Unit) {
+    open fun subscribe(onNext: (T?) -> Unit, onError: (Throwable?) -> Unit) {
         subscribe(onNext, onError, {})
     }
 
-    fun subscribe(onNext: (T?) -> Unit, onComplete: () -> Unit) {
+    open fun subscribe(onNext: (T?) -> Unit, onComplete: () -> Unit) {
         subscribe(onNext, {}, onComplete)
     }
 
-    fun subscribe(onNext: (T?) -> Unit) {
+    open fun subscribe(onNext: (T?) -> Unit) {
         subscribe(onNext, {}, {})
     }
 
-    fun onReceive(onNext: (T?) -> Unit, onError: (Throwable?) -> Unit, onComplete: () -> Unit) =
+    open fun onReceive(
+        onNext: (T?) -> Unit,
+        onError: (Throwable?) -> Unit,
+        onComplete: () -> Unit
+    ) =
         subscribe(onNext, onError, onComplete)
 
-    fun onReceive(onNext: (T?) -> Unit, onError: (Throwable?) -> Unit) =
+    open fun onReceive(onNext: (T?) -> Unit, onError: (Throwable?) -> Unit) =
         subscribe(onNext, onError, {})
 
-    fun onReceive(onNext: (T?) -> Unit, onComplete: () -> Unit) =
+    open fun onReceive(onNext: (T?) -> Unit, onComplete: () -> Unit) =
         subscribe(onNext, {}, onComplete)
 
-    fun onReceive(onNext: (T?) -> Unit) = subscribe(onNext)
+    open fun onReceive(onNext: (T?) -> Unit) = subscribe(onNext)
 
     /**
      * Filter and Map Events by Class
@@ -144,7 +159,7 @@ class EventStream<T : Any>(
     /**
      * Filter Events by vararg Classes
      */
-    fun isAnyOf(vararg args: KClass<out T>): EventStream<T>? {
+    open fun isAnyOf(vararg args: KClass<out T>): EventStream<T>? {
         return EventStream(
             filter { event ->
                 args.map { arg ->
@@ -158,7 +173,7 @@ class EventStream<T : Any>(
      * Filter emits only events from an
      * EventStream that satisfies a predicate function.
      */
-    fun filter(predicate: ((T?) -> Boolean)?): EventStream<T>? {
+    open fun filter(predicate: ((T?) -> Boolean)?): EventStream<T>? {
         return EventStream(observable?.filter(predicate))
     }
 
@@ -166,7 +181,7 @@ class EventStream<T : Any>(
      * Map transforms an EventStream by creating
      * a new EventStream through a projection function.
      */
-    fun <R : Any> map(transform: ((T?) -> R)?): EventStream<R>? {
+    open fun <R : Any> map(transform: ((T?) -> R)?): EventStream<R>? {
         return EventStream(observable?.map(transform))
     }
 
@@ -177,7 +192,7 @@ class EventStream<T : Any>(
      * Takes a predicate function as the sequence condition
      * and the length of the sequence to be considered.
      */
-    fun sequence(
+    open fun sequence(
         predicate: ((T?, T?) -> Boolean)?,
         count: Int?,
         skip: Int? = count
@@ -205,7 +220,7 @@ class EventStream<T : Any>(
      * the subscriber through a ComplexEvent object when
      * both EventStreams happen within a given time frame.
      */
-    fun <R : Any> merge(stream: EventStream<R>?): ComplexEvent? {
+    open fun <R : Any> merge(stream: EventStream<R>?): ComplexEvent? {
         val merged = Observable.merge(
             observable?.map { element -> Pair(element, (1 as Int?)) },
             stream?.observable?.map { element -> Pair(element, (2 as Int?)) }
@@ -220,7 +235,7 @@ class EventStream<T : Any>(
     /**
      * Window only emits events that happened within a given time frame.
      */
-    fun window(timespan: Long?, timeUnit: TimeUnit?): EventStream<T>? {
+    open fun window(timespan: Long?, timeUnit: TimeUnit?): EventStream<T>? {
         return EventStream(
             observable?.buffer(
                 timespan!!,
@@ -232,7 +247,7 @@ class EventStream<T : Any>(
      * Union merges two EventStreams into one EventStream
      * that emits events from both streams as they arrive.
      */
-    fun union(stream: EventStream<T>?): EventStream<T>? {
+    open fun union(stream: EventStream<T>?): EventStream<T>? {
         val merged = Observable.merge(
             observable,
             stream?.observable
@@ -245,7 +260,7 @@ class EventStream<T : Any>(
      * Filters the accumulated from all EventStreams that
      * exists in current stream but not in given one.
      */
-    fun not(stream: EventStream<T>?): EventStream<T>? {
+    open fun not(stream: EventStream<T>?): EventStream<T>? {
         val streamAccumulated: EventStream<MutableList<T>>? =
             EventStream(stream?.accumulator()?.observable?.startWithArray(arrayListOf()))
 
@@ -263,7 +278,7 @@ class EventStream<T : Any>(
      * Filters the accumulated from all EventStreams
      * that exists in current stream and in given one.
      */
-    fun intersect(stream: EventStream<T>?): EventStream<T>? {
+    open fun intersect(stream: EventStream<T>?): EventStream<T>? {
         val streamAccumulated: EventStream<MutableList<T>>? =
             EventStream(stream?.accumulator()?.observable?.startWithArray(arrayListOf()))
 
@@ -280,7 +295,7 @@ class EventStream<T : Any>(
     /**
      * Return a stream without duplicates.
      */
-    fun distinct(): EventStream<T>? {
+    open fun distinct(): EventStream<T>? {
         return EventStream(observable?.distinct())
     }
 
@@ -288,7 +303,7 @@ class EventStream<T : Any>(
      * Compare events by given comparator and return
      * a stream with all of then ordered by accordingly.
      */
-    fun <R : Comparable<R>> orderBy(comparison: ((T?) -> R?)): EventStream<List<T>>? {
+    open fun <R : Comparable<R>> orderBy(comparison: ((T?) -> R?)): EventStream<List<T>>? {
         val ordered = accumulator()?.map {
             it!!.sortedBy(comparison)
         }?.observable
@@ -300,7 +315,7 @@ class EventStream<T : Any>(
      * Compare events by given comparator and return
      * a stream with all of then grouped by accordingly.
      */
-    fun <R> groupBy(comparison: ((T?) -> R?)): EventStream<Map<R?, List<T>>>? {
+    open fun <R> groupBy(comparison: ((T?) -> R?)): EventStream<Map<R?, List<T>>>? {
         val grouped = accumulator()?.map {
             it!!.groupBy(comparison)
         }?.observable
@@ -311,7 +326,7 @@ class EventStream<T : Any>(
     /**
      * Scans the observable to get an accumulation of events as a List Stream.
      */
-    fun accumulator(): EventStream<MutableList<T>>? {
+    open fun accumulator(): EventStream<MutableList<T>>? {
         val accumulator = observable?.scan(
             mutableListOf<T>(),
             { accumulated, item ->
